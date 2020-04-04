3cc5232bad1680c5c6648dddc19a1fefae623e93
Eretol <befotd@gmail.com>
2020-04-04 22:46:37 +0200

    Laserbeam, min test scen, partikel folder

---
+++ Assets/FunctionActions/Laserbeam.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+[CreateAssetMenu()]
+public class Laserbeam : FunctionAction
+{
+    public int damage;
+
+    public override IEnumerator Act() {
+        Debug.Log("Play ParticleEffect");
+        for (int i = Target.Occupants.Count - 1; i >= 0; i--) {
+            Entity e = Target.Occupants[i];
+            Debug.Log("Damaging enemy: " + e.name);
+            e.Health -= damage;
+        }
+        yield return null;
+    }
+
+
+
+    public override IEnumerator Visualize() {
+
+        for(int i = 0; i<5; i++) {
+            List<Node> adjacentNodes = Origin.Edges.Select(e => e.To).ToList();
+            Node bestNode = adjacentNodes.First();
+            float bestDot = -1.0f;
+            foreach (Node node in adjacentNodes) {
+                float dot = Vector3.Dot((node.transform.position - Origin.transform.position).normalized,
+                                        (Target.transform.position - Origin.transform.position).normalized);
+                if (dot > bestDot) {
+                    bestDot = dot;
+                    bestNode = node;
+                }
+            }
+            Target = bestNode;
+            Act();
+            yield return new WaitForSeconds(0.1f);
+        }
+
+
+
+        yield return null;
+    }
+
+    public override void ResetVisualization() {
+        throw new System.NotImplementedException();
+    }
+
+
+    public override void AIDecision() {
+        throw new System.NotImplementedException();
+    }
+
+
+}
