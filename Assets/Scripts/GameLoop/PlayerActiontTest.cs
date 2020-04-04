6982244e1394d3ce80fce515126a272e6fd86fbc
Daniel Fors <daniel.lennart.fors@gmail.com>
2020-04-04 20:54:12 +0200

    path finding :)

--- Assets/Scripts/GameLoop/PlayerActiontTest.cs
+++ Assets/Scripts/GameLoop/PlayerActiontTest.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 
 public class PlayerActiontTest : MonoBehaviour
 {
+    public Node start;
+    public Node target;
     public void AddAction(FunctionAction action)
     {
         GameLoop.Instance.PlayerState.SetAction(action);
+    }
+    
+    public void DebugPath()
+    {
+        List<Node> path = Graph.Instance.ShortestPath(start, target);
+        foreach (Node p in path)
+        {
+            Debug.Log(p.name);
+        }
     }
 }
