fafecb0d735139284b13e8cef69c752e45de92c1
Daniel Fors <daniel.lennart.fors@gmail.com>
2020-04-05 12:44:21 +0200

    move arrow

--- Assets/FunctionActions/Move.cs
+++ Assets/FunctionActions/Move.cs
@@ -9,8 +9,10 @@ public class Move : FunctionAction
     public float ActLerpTime;
     public float VisualLerpTime;
     public int EnemyMoveDamage;
+    public Sprite arrow;
 
-    private GameObject visualization;
+    private GameObject visualizeChar;
+    private GameObject visualizeArrow;
 
     public override void Initialize(Entity actor)
     {
@@ -36,6 +38,11 @@ public class Move : FunctionAction
     }
     public override IEnumerator Act()
     {
+        if (visualizeChar != null)
+        {
+            Destroy(visualizeChar);
+            Destroy(visualizeArrow);
+        }
         float time = 0;
         Origin.RemoveOccupant(actor);
         while (time / VisualLerpTime <= 1f)
@@ -57,31 +64,43 @@ public class Move : FunctionAction
     }
     public override IEnumerator Visualize()
     {
-        if (visualization != null)
-            Destroy(visualization);
+        if (visualizeChar != null)
+        {
+            Destroy(visualizeChar);
+            Destroy(visualizeArrow);
+        }
 
-        visualization = Instantiate(actor.gameObject);
-        SpriteRenderer renderer = visualization.GetComponentInChildren<SpriteRenderer>();
+        visualizeChar = Instantiate(actor.gameObject);
+        SpriteRenderer renderer = visualizeChar.GetComponentInChildren<SpriteRenderer>();
         Color c = renderer.color;
         c.a = 0.65f;
         renderer.color = c;
 
+        visualizeArrow = new GameObject();
+        SpriteRenderer ren = visualizeArrow.AddComponent<SpriteRenderer>();
+        ren.sprite = arrow;
+        ren.color = Color.green;
+        visualizeArrow.transform.position = (Origin.transform.position * 0.7f + Target.transform.position * 0.3f);
+        if (Target.transform.position.x < Origin.transform.position.x)
+            ren.flipX = true;
+
         float time = 0;
         while(time / VisualLerpTime <= 1f)
         {
-            if (visualization == null)
+            if (visualizeChar == null)
                 break;
-            visualization.transform.position = Vector3.Lerp(Origin.transform.position, Target.transform.position, time / VisualLerpTime);
+            visualizeChar.transform.position = Vector3.Lerp(Origin.transform.position, Target.transform.position, time / VisualLerpTime);
             time += Time.deltaTime;
             yield return null;
         }
-        if (visualization != null)
-            Destroy(visualization);
     }
 
     public override void ResetVisualization()
     {
-        if (visualization != null)
-            Destroy(visualization);
+        if (visualizeChar != null)
+        {
+            Destroy(visualizeChar);
+            Destroy(visualizeArrow);
+        }
     }
 }
