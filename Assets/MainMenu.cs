81f4c8577c4e15db84ac3df1d6d87b1a6583ab96
molvin <akerstrom.perry@gmail.com>
2020-04-05 13:43:33 +0200

    asd

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    private void Start()
+    {
+        Persistance.Instance.Round = 0;
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+            SceneManager.LoadScene(1);
+
+        if (Input.GetMouseButtonDown(0))
+            SceneManager.LoadScene(1);
+    }
+}
