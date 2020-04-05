52da980f2825925ba347475d1337b510ec463e53
NiklasLand <niklas-landberg@hotmail.com>
2020-04-05 09:51:39 +0200

    scroll zoom

--- Assets/CameraMove.cs
+++ Assets/CameraMove.cs
@@ -4,11 +4,25 @@ using UnityEngine;
 
 public class CameraMove : MonoBehaviour
 {
+    float minSize = 2f;
+    float maxSize = 15f;
+    float sensitivity = 2f;
+
     public float StickFactor;
     void Update()
     {
         Vector2 pos = new Vector2(Input.mousePosition.x - Screen.width / 2, Input.mousePosition.y - Screen.height / 2) / StickFactor;
         transform.position = new Vector3(pos.x, pos.y, -10);
+        UpdateZoom();
+    }
 
+
+
+    void UpdateZoom()
+    {
+        float size = Camera.main.orthographicSize;
+        size -= Input.GetAxis("Mouse ScrollWheel") * sensitivity;
+        size = Mathf.Clamp(size, minSize, maxSize);
+        Camera.main.orthographicSize = size;
     }
 }
